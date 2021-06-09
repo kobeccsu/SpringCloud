9710698835ff4d72e3c88b0ebceed5bde45b112f
LeiZhou <Lei_Zhou@epam.com>
2021-06-09 16:46:04 +0800

    finish Gateway of the spring cloud

---
+++ SpringCloud/cloud-getway-getway9527/src/main/java/com/leizhou/springcloud/filter/MyLogGatewayFilter.java
@@ -0,0 +1,32 @@
+package com.leizhou.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+@Component
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println("来了一个用户" + new Date());
+
+        String name = exchange.getRequest().getQueryParams().getFirst("name");
+        if (name == null){
+            System.out.println("Illegal user");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
