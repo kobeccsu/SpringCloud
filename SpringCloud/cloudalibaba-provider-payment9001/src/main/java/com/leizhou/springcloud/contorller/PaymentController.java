492138b3c650e98f8434031086384b143e03f462
LeiZhou <Lei_Zhou@epam.com>
2021-06-11 16:46:52 +0800

    learn nacos

---
+++ SpringCloud/cloudalibaba-provider-payment9001/src/main/java/com/leizhou/springcloud/contorller/PaymentController.java
@@ -0,0 +1,18 @@
+package com.leizhou.springcloud.contorller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Long id){
+        return "Nacos registry, server port: " + serverPort + " your input id : " + id;
+    }
+}
